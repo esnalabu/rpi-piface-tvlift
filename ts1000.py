e8f2f5b4ab1c35a1404a3ffc798cf6104580a02e
Anders Myren <esnalabu@hotmail.com>
2016-12-11 17:14:04 +0000

    Rewrite: Use digital_read instead of eventlistener to increase stability.

--- ts1000.py
+++ ts1000.py
@@ -1,34 +1,47 @@
 #!/bin/python3
+# This script is for controlling a Venset TV-lift using power from an USB-port as trigger. Add a pulldown resistor between inputpin and common/ground/0V.
 
 import pifacedigitalio as p
 from time import sleep
 
-raiseduration = 3
-lowerduration = 3
-position = 0
+# Set input and output pins
+inputpin = 0	# Pin used for triggering lift
+raisepin = 0	# Pin used for raising lift
+lowerpin = 0	# Pin used for lowering lift
 
-def raise_lift(event):
+# Set default position and operating variables
+raiseduration = 3	# The time lift uses to reach to appropriate height
+lowerduration = 3	# The time lift uses from raised position to lowered position
+usb_status = 1		# Input of piface is active low, so this sets default usb_status to active indicating connected . I recommend connecting rpi/piface to a USB-port with no power when setting up to avoid raising an already raised lift.
+
+# Define functions raising and lowering TV-lift
+def raise_lift():
     print("Raising TV-lift for ", raiseduration, "s..."  )
-    event.chip.output_pins[0].turn_on()
+    p.digital_write(raisepin,1)
     sleep(raiseduration)
-    event.chip.output_pins[0].turn_off()
+    p.digital_write(raisepin,0)
     print("Done raising TV-lift!"  )
 
-def lower_lift(event):
+def lower_lift():
     print("Lowering TV-lift for ", lowerduration, "s..."  )
-    event.chip.output_pins[1].turn_on()
+    p.digital_write(lowerpin,1)
     sleep(raiseduration)
-    event.chip.output_pins[1].turn_off()
+    p.digital_write(lowerpin,0)
     print("Done lowering TV-lift!"  )
-    
 
+# Run, check for new status, raise/lower on change, sleep for stability
 if __name__ == "__main__":
-    
-    pifacedigital = p.PiFaceDigital()
-    inputpin = 0
+    p.init()					# Start interfacing with piface
+    pifacedigital = p.PiFaceDigital()		# Set piface address
+    p.core.digital_write_pullup(inputpin,0,0)	# Disable internal pullup resistor since we're using an "external" pulldown resistor
 
-    listener = p.InputEventListener(chip=pifacedigital)
-    listener.register(inputpin, p.IODIR_OFF, raise_lift)
-    listener.register(inputpin, p.IODIR_ON, lower_lift)
-    listener.activate()
-
+    while(True):
+        new_status = p.digital_read(inputpin)
+        if new_status != usb_status:
+            if new_status == 0:
+                raise_lift()
+                sleep(3)
+            elif new_status == 1:
+                lower_lift()
+                sleep(3)
+        usb_status = new_status
